5df637c8c8fb93cb78df28c2f303ea22da2256f3
Maria <Maria@192.168.1.49>
2018-04-02 01:51:51 +0200

    Ejemplo práctico del Objeto getUserMedia

---
+++ public_html/js/getUserMedia.js
@@ -0,0 +1,24 @@
+var video = document.querySelector('#video');
+
+navigator.webcam = (
+    navigator.msGetUserMedia || //Para IE
+    navigator.mosGetUserMedia ||   //Para FireFox
+    navigator.webkitGetUserMedia ||
+    navigator.getUserMedia
+);
+
+//navigator.webcam({options}. success, error) --> Éstos son los 3 parámetros necesarios
+
+function success(stream)  //stream -> transmisión
+{
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+}
+
+function error(error)
+{
+    alert('Error al acceder a la webcam');
+    console.log(error);
+}
+
+navigator.webcam({ video: true, audio:true }, success, error);
